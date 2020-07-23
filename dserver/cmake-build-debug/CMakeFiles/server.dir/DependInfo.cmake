4b362ebec05663cf6fc49768e4bfeb2d3177273f
thedushn <thedushn@email.com>
2020-07-23 21:07:00 +0200

    ggsdfd

---
+++ dserver/cmake-build-debug/CMakeFiles/server.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/dushn/Diplomski/dserver/cpu_usage.c" "/home/dushn/Diplomski/dserver/cmake-build-debug/CMakeFiles/server.dir/cpu_usage.c.o"
+  "/home/dushn/Diplomski/dserver/devices.c" "/home/dushn/Diplomski/dserver/cmake-build-debug/CMakeFiles/server.dir/devices.c.o"
+  "/home/dushn/Diplomski/dserver/functions.c" "/home/dushn/Diplomski/dserver/cmake-build-debug/CMakeFiles/server.dir/functions.c.o"
+  "/home/dushn/Diplomski/dserver/interrupts.c" "/home/dushn/Diplomski/dserver/cmake-build-debug/CMakeFiles/server.dir/interrupts.c.o"
+  "/home/dushn/Diplomski/dserver/main.c" "/home/dushn/Diplomski/dserver/cmake-build-debug/CMakeFiles/server.dir/main.c.o"
+  "/home/dushn/Diplomski/dserver/memory_usage.c" "/home/dushn/Diplomski/dserver/cmake-build-debug/CMakeFiles/server.dir/memory_usage.c.o"
+  "/home/dushn/Diplomski/dserver/network_bandwith.c" "/home/dushn/Diplomski/dserver/cmake-build-debug/CMakeFiles/server.dir/network_bandwith.c.o"
+  "/home/dushn/Diplomski/dserver/task_manager.c" "/home/dushn/Diplomski/dserver/cmake-build-debug/CMakeFiles/server.dir/task_manager.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/gtk-3.0"
+  "/usr/include/at-spi2-atk/2.0"
+  "/usr/include/at-spi-2.0"
+  "/usr/include/dbus-1.0"
+  "/usr/lib/x86_64-linux-gnu/dbus-1.0/include"
+  "/usr/include/gio-unix-2.0"
+  "/usr/include/mirclient"
+  "/usr/include/mircore"
+  "/usr/include/mircookie"
+  "/usr/include/cairo"
+  "/usr/include/pango-1.0"
+  "/usr/include/harfbuzz"
+  "/usr/include/atk-1.0"
+  "/usr/include/pixman-1"
+  "/usr/include/freetype2"
+  "/usr/include/libpng12"
+  "/usr/include/gdk-pixbuf-2.0"
+  "/usr/include/glib-2.0"
+  "/usr/lib/x86_64-linux-gnu/glib-2.0/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ dserver/cmake-build-debug/CMakeFiles/server.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/server.dir/main.c.o"
+  "CMakeFiles/server.dir/cpu_usage.c.o"
+  "CMakeFiles/server.dir/devices.c.o"
+  "CMakeFiles/server.dir/functions.c.o"
+  "CMakeFiles/server.dir/interrupts.c.o"
+  "CMakeFiles/server.dir/memory_usage.c.o"
+  "CMakeFiles/server.dir/network_bandwith.c.o"
+  "CMakeFiles/server.dir/task_manager.c.o"
+  "server.pdb"
+  "server"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
